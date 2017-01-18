cf5ed79d464751e11a2c27f1c9a050d1b47b8bba
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2017-01-18 19:02:01 +0100

    erased useless

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 
-#include "src/generic/math/polygon.hpp"
-#include "src/generic/math/matrix.hpp"
-
 int main ()
 {
-    pag::bs::math::geometric_point<int,3> test_point {1,1,1};
-    pag::bs::math::geometric_point<int,3> vector {0,0,8};
-    pag::bs::math::matrix<int,3> mat(vector);
-    std::cout << "matrix multiplication : ";
-
-    display_ar (mat * test_point,std::cout);
-
-    std::endl(std::cout);
-
-
-
-
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
