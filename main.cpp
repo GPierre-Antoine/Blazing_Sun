e63f74a8753fb21709df41b48538d89683a7defa
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2017-01-18 01:37:54 +0100

    test of polygon and geometric point iterators

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 
 int main ()
 {
+    math::polygon<int,2> blabla {{0,0},{0,1},{1,1},{1,0}};
+    for (const auto & i : blabla)
+    {
+        for (const auto & j : i)
+        {
+            std::cout << j << ", ";
+        }
+        std::cout << std::endl;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
