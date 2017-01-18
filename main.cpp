d71c110206bde1928f838d33e7f81212e2228685
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2017-01-18 16:32:37 +0100

    move ctor has weird behaviour

--- main.cpp
+++ main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
-#include <array>
-#include <numeric>
+
 #include "src/generic/math/polygon.hpp"
 #include "src/generic/math/matrix.hpp"
-
-
-typedef pag::bs::math::matrix<int,3> normatrix;
-
-template <typename T>
-std::ostream& display_ar(const T& sec, std::ostream& i)
-{
-    for (const auto&e : sec)
-    {
-        std::cout << e << ", ";
-    }
-    return i;
-}
-template <typename T>
-std::ostream& display_mat (const T& a,std::ostream&i)
-{
-    for (const auto&sec : a)
-    {
-        display_ar (sec,i);
-        std::endl(std::cout);
-    }
-    return i;
-}
-normatrix&& test()
-{
-    pag::bs::math::geometric_point<int,3> pt{8,8,8};
-    normatrix t(pt);
-    normatrix t2(pt);
-
-    return std::move(t);
-}
-
 
 int main ()
 {
@@ -47,12 +14,6 @@ int main ()
 
     std::endl(std::cout);
 
-    std::cout << "matrix swap" << std::endl;
-    normatrix n = test ();
-
-    std::cout << "matrix swaped" << std::endl;
-
-    display_mat (n,std::cout);
 
 
 
