bd7b81280f1a50f3b124ee1fcb3f9e7e68abdfa9
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2017-01-20 00:47:52 +0100

    math constants (FNVa ...)

---
+++ src/generic/math/constants.h
@@ -0,0 +1,21 @@
+//
+// Created by Pierre-Antoine on 18/01/2017.
+//
+
+#ifndef BLAZING_SUN_CONSTANTS_H
+#define BLAZING_SUN_CONSTANTS_H
+
+namespace pag
+{
+    namespace bs
+    {
+        namespace math
+        {
+            constexpr double euler_mascheroni_constant = 0.5772156649;
+            constexpr size_t FNV_prime = 14695981039346656037u;
+            constexpr size_t FNV_offset_value = 1099511628211u;
+        }
+    }
+}
+
+#endif //BLAZING_SUN_CONSTANTS_H
