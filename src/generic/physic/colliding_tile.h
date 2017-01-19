c8fa265976be0fb636f03f73f1f31a8bda032bfc
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2017-01-20 00:53:58 +0100

    first draft of colliding tile

--- src/generic/physic/colliding_tile.h
+++ src/generic/physic/colliding_tile.h
@@ -5,6 +5,37 @@
 #ifndef BLAZING_SUN_COLLIDING_TILE_H
 #define BLAZING_SUN_COLLIDING_TILE_H
 
+#include <memory>
+#include <vector>
 
+#include "../math/point.hpp"
+
+namespace pag
+{
+    namespace bs
+    {
+        namespace physic
+        {
+
+            template <typename T, unsigned char U>
+            class colliding_tile
+            {
+                typedef typename pag::bs::math::geometric_point<T,U>    geo_pt;
+                typedef typename pag::bs::math::polygon<T,U>            poly;
+                typedef typename std::shared_ptr<poly>                  poly_ptr;
+
+
+                const geo_pt first;
+                const geo_pt last;
+
+                std::vector<poly_ptr> polygons;
+
+            public:
+
+
+            };
+        }
+    }
+}
 
 #endif //BLAZING_SUN_COLLIDING_TILE_H
