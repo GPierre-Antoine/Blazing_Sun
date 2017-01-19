035715a254545f09d4cf0d7c733e533bd9f0e1b7
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2017-01-20 00:53:10 +0100

    pythagora + zipf's law

--- src/generic/math/functions.cpp
+++ src/generic/math/functions.cpp
@@ -1,6 +1,30 @@
 //
 // Created by Pierre-Antoine on 18/01/2017.
 //
+#include <iostream>
 
 #include "functions.h"
 
+
+double pag::bs::math::zipf_law (size_t pool_size, const size_t number_of_chucks, size_t chunck_index)
+{
+    using std::cout;
+    using std::endl;
+
+    double *lookup = new double[number_of_chucks];
+    double value = 0;
+    for (unsigned i{1};i<number_of_chucks+1;++i)
+    {
+        value +=(1./i);
+        lookup[i-1] = value;
+    }
+    double eax = 0;
+    if (chunck_index)
+    {
+        eax = lookup[chunck_index] - lookup[chunck_index-1];
+    }
+    else
+        eax=1;
+    delete[] lookup;
+    return (eax/value)*pool_size;
+}
--- src/generic/math/functions.h
+++ src/generic/math/functions.h
@@ -2,9 +2,8 @@
 #ifndef BLAZING_SUN_FUNCTIONS_H
 #define BLAZING_SUN_FUNCTIONS_H
 
-#include "point.hpp"
 #include <cmath>
-
+#include "constants.h"
 
 namespace pag
 {
@@ -14,7 +13,7 @@ namespace pag
         namespace math
         {
             template <typename iter_t>
-            double pythagore (const iter_t begin,const iter_t & end, iter_t dopple)
+            double pythagora (const iter_t begin, const iter_t & end, iter_t dopple)
             {
 
                 double eax = 0;
@@ -25,41 +24,14 @@ namespace pag
                 return sqrt (eax);
             }
 
+            double zipf_law (size_t pool_size, const size_t number_of_chucks, size_t chunck_index);
+
         }//math
 
     }//bs
 }//pag
 
-namespace std
-{
-    constexpr size_t FNV_prime = 14695981039346656037u;
-    constexpr size_t FNV_offset_value = 1099511628211u;
 
-    template <>
-    template <typename T, unsigned char D>
-    struct hash<pag::bs::math::geometric_point<T,D>>
-    {
-
-        size_t operator ()(const pag::bs::math::geometric_point<T,D>& target)
-        {
-            size_t hash_val = FNV_prime;
-            for (const auto &i: target) {hash_val ^= i; hash_val*= FNV_offset_value; }
-            return hash_val;
-        }
-    };
-
-    template <>
-    template <typename T, unsigned char D>
-    struct hash<pag::bs::math::polygon<T,D>>
-    {
-        size_t operator ()(const pag::bs::math::polygon<T,D>& target)
-        {
-            size_t hash_val = FNV_prime;
-            for (const auto &i: target) {hash_val ^= hash<pag::bs::math::geometric_point<T,D>>(i); hash_val*= FNV_offset_value; }
-            return hash_val;
-        }
-    };
-}
 
 
 #endif //BLAZING_SUN_FUNCTIONS_H
